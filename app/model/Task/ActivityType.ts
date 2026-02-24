5629f10717f70438b1cb3ddf0f0a758e69c24c95
David Valek <david.valek17@gmail.com>
2026-02-24 13:09:43 +0100

    Add doc comments

--- app/model/Task/ActivityType.ts
+++ app/model/Task/ActivityType.ts
@@ -1,5 +1,20 @@
+/**
+ * Defines the types of activities that can be performed in a task.
+ */
 export enum ActivityType {
+    /**
+     * Represents an activity where the user needs to repair a component or system. 
+     * This could involve fixing a broken part, restoring functionality, or resolving an issue.
+     */
     REPAIR = 'repair',
+
+    /**
+     * Represents an activity where the user needs to select a component or components which are faulty.
+     */
     SELECT = 'select',
+
+    /**
+     * Represents an activity where the user needs to select options related to a component or system.
+     */
     SELECT_OPTIONS = 'select-options',
 }
