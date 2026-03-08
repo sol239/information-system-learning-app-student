7b46a1a90c201bb416dd8422be34c961449ef744
David Valek <david.valek17@gmail.com>
2026-03-08 19:45:17 +0100

    Create helper class to work with sql.js db

---
+++ app/utils/DatabaseWrapper.ts
@@ -0,0 +1,46 @@
+import initSqlJs, { type Database } from 'sql.js';
+import { DatabaseHandler } from './DatabaseHandler';
+import { OperationResultType } from './OperationResultType';
+export class DatabaseWrapper {
+    /**
+       * The special sync number used for forcing reactivity updates when the database is initialized or updated. 
+       * Incrementing this number can be used to trigger reactivity in Vue components that depend on the database state.
+       */
+    public dbNumber: number = 0;
+
+    /**
+     * Whether is the database for this information system initialized.
+     */
+    public async isDatabaseInitialized(): Promise<boolean> {
+        console.log("Checking if database is initialized");
+        if (this.sqlJsDatabase === null) return false;
+        const result = await DatabaseHandler.getTableNames(this.sqlJsDatabase);
+        return result.result === OperationResultType.SUCCESS;
+    }
+
+    private constructor(
+        public sqlJsDatabase: Database | null,
+        public binaryData: Uint8Array | null
+    ) { }
+
+    static fromInstance(db: Database): DatabaseWrapper {
+        return new DatabaseWrapper(db, null);
+    }
+
+    static fromBinary(data: Uint8Array): DatabaseWrapper {
+        return new DatabaseWrapper(null, data);
+    }
+
+    public async initializeDatabase(): Promise<void> {
+        if (this.sqlJsDatabase !== null) return;
+        if (this.binaryData === null) return;
+
+        const SQL = await initSqlJs({
+            locateFile: () => '/information-system-learning-app/sql-wasm.wasm'
+        });
+
+        this.sqlJsDatabase = new SQL.Database(this.binaryData);
+        this.dbNumber++;
+        console.log("Database initialized");
+    }
+}
