17bf0cc27b961d5e4d888bbfbba4b0d4bd7c02aa
David Valek <david.valek17@gmail.com>
2026-03-08 19:43:39 +0100

    Create type to store variable definitions

---
+++ app/core/TableMap.ts
@@ -0,0 +1,16 @@
+import type { VariableType } from "~/model/types/VariableType";
+
+export class TableMap {
+    constructor(public tableName: string | null, public columnName: string | null, public variableAsName: string, public columnType: ColumnType, public tableAlias?: string) { }
+
+    public toString(): string {
+        if (this.tableName === null || this.columnName === null) {
+            return `${this.variableAsName}`;
+        }
+        if (this.tableAlias) {
+            return `${this.tableAlias}.${this.columnName} as ${this.variableAsName}`;
+        } else {
+            return `${this.tableName}.${this.columnName} as ${this.variableAsName}`;
+        }
+    }
+}
